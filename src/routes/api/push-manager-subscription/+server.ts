0258f701408f0a07b41b3ea7cfbfca28b3ae837c
Jack O'Hara <jack@jackohara.io>
2022-12-30 13:51:13 +0000

    Add some logging to the backend function

--- src/routes/api/push-manager-subscription/+server.ts
+++ src/routes/api/push-manager-subscription/+server.ts
@@ -2,21 +2,33 @@ import { error } from "@sveltejs/kit";
 import type { RequestEvent } from "./$types";
 
 export async function POST({ locals, fetch, request }: RequestEvent): Promise<Response> {
+    console.log('In the api function')
     const session = await locals.getSession()
 
     if (!session) {
         throw error(401, 'You must be logged in to use this endpoint')
     }
 
+    console.log('Valid session')
+
     if (!('idToken' in session)) {
         throw error(500, 'Could not find valid access token in session')
     }
 
-    return fetch('https://jg0e9kasq9.execute-api.eu-west-1.amazonaws.com/prod/push-manager-subscription', {
-        method: 'POST',
-        headers: {
-            Authorization: `Bearer ${session.idToken}`
-        },
-        body: await request.text()
-    })
+    console.log('Found ID token')
+
+    try {
+        const response = await fetch('https://jg0e9kasq9.execute-api.eu-west-1.amazonaws.com/prod/push-manager-subscription', {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${session.idToken}`
+            },
+            body: await request.text()
+        })
+
+        console.log('response:', response)
+        return response
+    } catch (e) {
+        console.log('error:', e)
+    }
 }
